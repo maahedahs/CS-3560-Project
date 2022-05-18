1d4fbf23a9978320acd8e7e58ccf5e7f4820352d
maahedahs <72372121+maahedahs@users.noreply.github.com>
2022-05-17 20:23:07 -0700

    Added Viewer class

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # CS 3560 Project
 # Group Members: Yennhi Dang, Maahedah Sadiq, Sam De Raimondo, Urjaa Ghimirey, Vraj Patel, Jacob Jung
 
@@ -6,67 +5,35 @@
 from pss import PSS
 from task import Task
 from schedule import Schedule
+from viewer import Viewer
 import sys
 
 if __name__ == '__main__':
+    viewer = Viewer()
+    pss = PSS()
 
     while 1:
-        print("\nWelcome to the Personal Scheduling System.")
-        print("1. Create a task")
-        print("2. Delete a task")
-        print("3. Edit a task")
-        print("4. Write schedule to a file")
-        print("5. Read schedule from a file")
-        print("6. View a schedule")
-        print("7. Exit")
-
+        viewer.display_main_menu()
         user_selection = int(input("\nEnter your selection: "))
+        
         if user_selection == 1:
             choose_task_type = input("Enter 'T' for transient task, 'R' for recurring task, or 'A' for anti task: ")
             if choose_task_type == 'T' or choose_task_type == 't':
-                task_name = input("Enter the task name you would like to create: ")
-                task_type = input("Enter the type of task: Visit, Shopping, or Appointment for Transient task: ")
-                task_start_date = input("Enter start date for the task: ")
-                task_start_time = float(input("Enter start time for the task: "))
-                task_duration = float(input("Enter duration for the task: "))
-
-                ######
-                #@TODO: pass all the task attributes to create task method 
-                ######
-
+                new_task = viewer.create_transient_task()
+                pss.add_task(new_task)    
             elif choose_task_type == 'R' or choose_task_type == 'r':
-                task_name = input("Enter the task name you would like to create: ")
-                task_type = input("Enter the type of task: Class, Study, Sleep, Exercise, Work, or Meal for Recurring task: ")
-                task_start_date = input("Enter start date for the task: ")
-                task_start_time = float(input("Enter start time for the task: "))
-                task_duration = float(input("Enter duration for the task: "))
-                task_end_date = input("Enter end date of the task: ")
-                task_frequency = input("Enter task frequency as 1(Daily) or 7(Weeky): ")
-
-                ######
-                #@TODO: pass all the task attributes to create task method 
-                ######
-
+                new_task = viewer.create_recurring_task()
+                pss.add_task(new_task)
             elif choose_task_type == 'A' or choose_task_type == 'a':
-                task_name = input("Enter the task name you would like to create for an Anti task: ")
-                task_type = input("Enter the type of task: Cancellation for Anti task:  ")
-                task_start_date = input("Enter start date for the anti task: ")
-                task_start_time = float(input("Enter start time for the anti task: "))
-                task_duration = float(input("Enter duration for the anti task: "))
-
-                ######
-                #@TODO: pass all the task attributes to create task method 
-                ######
+                new_task = viewer.create_anti_task()
+                pss.add_task(new_task)
             else:
                 print("\nError: Please choose your option between Transient task, Recurring task, or Anti task.")
-
+        
         elif user_selection == 2:
-
-            ######
-            #@TODO: Get user input and pass to remove_task
-            ######
-
-            pass
+            task_name = viewer.remove_task()
+            task_to_remove = pss.find_task(task_name)
+            pss.remove_task(task_to_remove)
 
         elif user_selection == 3:
 
